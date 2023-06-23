d389e3587d651c45ad5590bdf75e831f4c8f7ecd
Paulinachx100176 <128227114+Paulinachx100176@users.noreply.github.com>
2023-06-23 21:48:43 +0200

    Add files via upload

---
+++ cwiczenie_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę elementów ciągu Fibonacciego do wygenerowania:");
+        string input = Console.ReadLine();
+        int n;
+
+        while (!int.TryParse(input, out n) || n < 1)
+        {
+            Console.WriteLine("Nieprawidłowa liczba. Podaj liczbę większą od zera:");
+            input = Console.ReadLine();
+        }
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(a);
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        Console.ReadKey();
+    }
+}
